e43b2ae4cb8fe647df43309f6a3822c637c08383
Hero <211014@st.yoshida-g.ac.jp>
2022-07-07 13:22:42 +0900

    mail_register_comiit

---
+++ resources/views/auth/verification.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <h1>ユーザ登録</h1>
+                    <p>ユーザ登録を行いました．</p>
+                    <p>確認のためにEメールを送信しましたので，メールのリンクをクリックしてください．</p>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/auth/verifyemail/{token}', 'Auth\RegisterController@verify');
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
