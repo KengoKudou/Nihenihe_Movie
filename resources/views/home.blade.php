20fff62535c72a62f04a9b619f430151806a5c95
KengoKudo <kng-kudo@yoshida-g.ac.jp>
2022-07-15 16:03:42 +0900

    @yabuchan_fix

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,34 +1,33 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">{{ __('Dashboard') }}</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+                    <div class="card-body">
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                        @endif
 
-                    {{ __('You are logged in!') }}
-                </div>
-                <div>
-                    <form action="/new_movies_send" method="post" enctype="multipart/form-data" accept-charset="video/*">
-                        @csrf
-                        <p>&nbsp;</p>
-                        <p>アップロード</p>
-                        <input type="file" name="post_img">
-                        <p>&nbsp;</p>
-                        <input type="submit">
-                    </form>
+                        {{ __('You are logged in!') }}
+                    </div>
+                    <div>
+                        <form action="/new_movies_send" method="post" enctype="multipart/form-data"
+                              accept-charset="video/*">
+                            @csrf
+                            <p>&nbsp;</p>
+                            <p>アップロード</p>
+                            <input type="file" name="post_img">
+                            <p>&nbsp;</p>
+                            <input type="submit">
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
-::q!
-/\/c
