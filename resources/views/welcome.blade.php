a68ad01b5615474e93a4e16500e9db81d120ad8d
KengoKudo <kng-kudo@yoshida-g.ac.jp>
2022-07-21 13:29:40 +0900

    @main.css&welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -22,8 +22,22 @@
                 <span class="Contributor_Name">
                     投稿者ネーム
                 </span>
-
             </a>
+        </div>
+        <div class="Advertisement">
+            <a class="Advertisement_nishigata"href="storage/img/advertisement/ポートフォリオ_西潟.pdf">
+                <img src="{{ asset('storage/img/advertisement/カヤック.png') }}" class="Advertisement_Icon" alt="面白法人カヤック_西潟ポートフォリオ">
+            </a>
+            <br>
+            <a class="Advertisement_kudo"href="storage/img/advertisement/ポートフォリオ_工藤.pdf">
+                <img src="{{ asset('storage/img/advertisement/クリプトン.png') }}" class="Advertisement_Icon" alt="クリプトン・フューチャー・メディア_工藤ポートフォリオ">
+            </a>
+        </div>
+    </div>
+    <div class="PickUp">
+        <div class="PickUp_Container">
+            <div class="PickUp_Container_Thumbnail">
+            </div>
         </div>
     </div>
 @endsection
