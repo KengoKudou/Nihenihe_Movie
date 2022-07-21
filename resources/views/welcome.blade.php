3ed8a9c7ad68deecf3fb0b1a33532a05acc62ded
KengoKudo <kng-kudo@yoshida-g.ac.jp>
2022-07-21 14:16:07 +0900

    @main.css&welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,15 +11,15 @@
             </a>
         </div>
         <div class="Description">
-            <div>
+            <div class="Description_Comment">
                 概要宣伝コメント
             </div>
-            <div>
+            <div class="Description_Title">
                 動画タイトル
             </div>
-            <a class="Contributor">
+            <a class="Description_Contributor">
                 <img src="{{ asset('storage/img/thumbnail/K_pink.png') }}" class="Contributor_Icon">
-                <span class="Contributor_Name">
+                <span class="Description_Contributor_Name">
                     投稿者ネーム
                 </span>
             </a>
@@ -34,10 +34,111 @@
             </a>
         </div>
     </div>
+
     <div class="PickUp">
         <div class="PickUp_Container">
-            <div class="PickUp_Container_Thumbnail">
+            <img src="{{ asset('storage/img/PickUp_Thumbnail/hoge1.png') }}" class="PickUp_Container_Thumbnail">
+            <div class="PickUp_Container_Title">
+                ピックアップタイトル
             </div>
+            <div class="PickUp_Container_Overview">
+                ピックアップ概要
+            </div>
+            <img src="{{ asset('storage/img/thumbnail/K_white.png') }}" class="PickUp_Container_Icon">
+            <span class="PickUp_Container_Name">
+                    ピックアップ投稿者ネーム
+                </span>
+        </div>
+        <div class="PickUp_Container">
+            <img src="{{ asset('storage/img/PickUp_Thumbnail/hoge2.png') }}" class="PickUp_Container_Thumbnail">
+            <div class="PickUp_Container_Title">
+                ピックアップタイトル
+            </div>
+            <div class="PickUp_Container_Overview">
+                ピックアップ概要
+            </div>
+            <img src="{{ asset('storage/img/thumbnail/K_white.png') }}" class="PickUp_Container_Icon">
+            <span class="PickUp_Container_Name">
+                    ピックアップ投稿者ネーム
+                </span>
+        </div>
+        <div class="PickUp_Container">
+            <img src="{{ asset('storage/img/PickUp_Thumbnail/hoge3.png') }}" class="PickUp_Container_Thumbnail">
+            <div class="PickUp_Container_Title">
+                ピックアップタイトル
+            </div>
+            <div class="PickUp_Container_Overview">
+                ピックアップ概要
+            </div>
+            <img src="{{ asset('storage/img/thumbnail/K_white.png') }}" class="PickUp_Container_Icon">
+            <span class="PickUp_Container_Name">
+                    ピックアップ投稿者ネーム
+                </span>
+        </div>
+        <div class="PickUp_Container">
+            <img src="{{ asset('storage/img/PickUp_Thumbnail/hoge4.png') }}" class="PickUp_Container_Thumbnail">
+            <div class="PickUp_Container_Title">
+                ピックアップタイトル
+            </div>
+            <div class="PickUp_Container_Overview">
+                ピックアップ概要
+            </div>
+            <img src="{{ asset('storage/img/thumbnail/K_white.png') }}" class="PickUp_Container_Icon">
+            <span class="PickUp_Container_Name">
+                    ピックアップ投稿者ネーム
+                </span>
+        </div>
+        <div class="PickUp_Container">
+            <img src="{{ asset('storage/img/PickUp_Thumbnail/hoge5.png') }}" class="PickUp_Container_Thumbnail">
+            <div class="PickUp_Container_Title">
+                ピックアップタイトル
+            </div>
+            <div class="PickUp_Container_Overview">
+                ピックアップ概要
+            </div>
+            <img src="{{ asset('storage/img/thumbnail/K_white.png') }}" class="PickUp_Container_Icon">
+            <span class="PickUp_Container_Name">
+                    ピックアップ投稿者ネーム
+                </span>
+        </div>
+        <div class="PickUp_Container">
+            <img src="{{ asset('storage/img/PickUp_Thumbnail/hoge6.png') }}" class="PickUp_Container_Thumbnail">
+            <div class="PickUp_Container_Title">
+                ピックアップタイトル
+            </div>
+            <div class="PickUp_Container_Overview">
+                ピックアップ概要
+            </div>
+            <img src="{{ asset('storage/img/thumbnail/K_white.png') }}" class="PickUp_Container_Icon">
+            <span class="PickUp_Container_Name">
+                    ピックアップ投稿者ネーム
+                </span>
+        </div>
+        <div class="PickUp_Container">
+            <img src="{{ asset('storage/img/PickUp_Thumbnail/hoge7.png') }}" class="PickUp_Container_Thumbnail">
+            <div class="PickUp_Container_Title">
+                ピックアップタイトル
+            </div>
+            <div class="PickUp_Container_Overview">
+                ピックアップ概要
+            </div>
+            <img src="{{ asset('storage/img/thumbnail/K_white.png') }}" class="PickUp_Container_Icon">
+            <span class="PickUp_Container_Name">
+                    ピックアップ投稿者ネーム
+                </span>
+        </div>
+        <div class="PickUp_Container">
+            <img src="{{ asset('storage/img/PickUp_Thumbnail/hoge8.png') }}" class="PickUp_Container_Thumbnail">
+            <div class="PickUp_Container_Title">
+                ピックアップタイトル
+            </div>
+            <div class="PickUp_Container_Overview">
+                ピックアップ概要
+            </div>
+            <img src="{{ asset('storage/img/thumbnail/K_white.png') }}" class="PickUp_Container_Icon">
+            <span class="PickUp_Container_Name">
+                    ピックアップ投稿者ネーム
+                </span>
         </div>
     </div>
 @endsection
