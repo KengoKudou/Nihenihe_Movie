a3e7b5705fc8d470e068cfe217298d8735a5c9cb
KengoKudo <kng-kudo@yoshida-g.ac.jp>
2022-07-21 14:29:39 +0900

    @movie.blade.php

--- resources/views/movie/movie.blade.php
+++ resources/views/movie/movie.blade.php
@@ -1,11 +1,19 @@
 @extends('layouts.app')
 @section('content')
-    <div class="parent">
-        <div class="bigMovie">
+    <div class="Parent_Movie">
+        <div class="Movie_Title">
+            動画タイトル
+        </div>
+        <div class="Movie_Overview">
+            動画説明欄
+        </div>
+        <div class="Movie">
             <video controls autoplay width="80%" height="auth">
-                <source src="{{ asset('storage/post_movies/A8Hx7m2aOFCG8yNuzy0k1KLznumUeSatAq1nVJ2U.mp4') }}"
-                        type="video/mp4">
+                <source src="{{ asset('storage/post_movies/Q2Frsr5QEp88XwLifQaJtpcCIpDD9pGwrrRucpW4.mp4') }}" type="video/mp4">
             </video>
+        </div>
+        <div class="ad">
+            <!-- 広告ここに入れようかな -->
         </div>
     </div>
 @endsection
