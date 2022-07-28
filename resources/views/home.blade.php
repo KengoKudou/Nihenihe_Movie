3abede75ee7b2ac400f9729f17fbe61e1f91cd9a
KengoKudo <kng-kudo@yoshida-g.ac.jp>
2022-07-28 12:10:13 +0900

    @home&main

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,10 +15,31 @@
             <button type="button" onclick="alert('動画検索')" class="Video_Search_FollowButton">検索</button>
         </div>
     </div>
-
     <div class="Top_MyPage">
+        <div class="Profile">
+            <div class="Profile_Icon">
+                <img src="{{ asset('storage/img/thumbnail/K_white.png') }}" class="Profile_Icon" alt="">
+            </div>
+            <div class="Profile_Name">
+                名前
+            </div>
+            <div class="Profile_Id">
+                ID
+            </div>
+            <div class="Profile_Password">
+                パスワード
+            </div>
+            <div class="Profile_Address">
+                アドレス
+            </div>
+            <div class="Profile_Introduction">
+                紹介文
+            </div>
+        </div>
         <div class="MyPage_Upload_Button">
-            <button type="button" class="Video_Creator_FollowButton" onclick="location.href='http://localhost:8000/upload'">アップロード</button>
+            <button type="button" class="Video_Creator_FollowButton"
+                    onclick="location.href='http://localhost:8000/upload'">アップロード
+            </button>
         </div>
     </div>
 @endsection
