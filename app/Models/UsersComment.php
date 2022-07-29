cf323e906fdebbf49949382c40feccf991f28747
qlfa <x_yabuta@icloud.com>
2022-07-29 09:50:46 +0900

    Fix ModelData.

--- app/Models/UsersComment.php
+++ app/Models/UsersComment.php
@@ -6,4 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class UsersComment extends Model
 {
+    protected $fillable = ['comment'];
+
+    public function insert_data($comment){
+        return $this->create([
+            'comment' => $comment
+        ]);
+    }
+
+    public function get_comment($name){
+        return $this
+            ->where('name', $name)
+            ->first(['comment']);
+    }
 }
