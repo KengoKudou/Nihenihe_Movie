711c0a09fd25af1aed463ad3b147bf167cbd6d5a
qlfa <x_yabuta@icloud.com>
2022-08-01 14:25:39 +0900

    fix 'Artwork.php'

--- app/Models/Artwork.php
+++ app/Models/Artwork.php
@@ -54,11 +54,8 @@ class Artwork extends Model
     private function get_path($data)
     {
         $user_path = 'storage/users/' . $data['name'] . '/' . $data['artwork_num'] . '/';
-        $movie_name = glob($user_path . 'movie.*');
-        $thumbnail_name = glob($user_path . 'thumbnail.*');
-
-        $movie_path = $user_path . $movie_name[0];
-        $thumbnail_path = $user_path . $thumbnail_name[0];
+        $movie_path = glob($user_path . 'movie.*');
+        $thumbnail_path = glob($user_path . 'thumbnail.*');
 
         $paths['movie_path'] = $movie_path;
         $paths['thumbnail_path'] = $thumbnail_path;
