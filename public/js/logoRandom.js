3e4b5aefff031de2f578807259eae2f2b104b14c
KengoKudo <kng-kudo@yoshida-g.ac.jp>
2022-08-22 10:55:17 +0900

    logoバグ修正(width修正）

--- public/js/logoRandom.js
+++ public/js/logoRandom.js
@@ -3,5 +3,5 @@ var randomList = ["storage/logo/nihenihe.gif",
     "storage/logo/nihenihedouga_02.png",
     "storage/logo/nihenihedouga_logo_01.png"];
 var logo_num = Math.floor(Math.random() * randomList.length);
-var printHtml = '<img src=' + randomList[logo_num] + ' width="100" height="100" alt="ランダム画像">';
+var printHtml = '<img src=' + randomList[logo_num] + ' width="auto" height="100" alt="ランダム画像">';
 document.write(printHtml);
