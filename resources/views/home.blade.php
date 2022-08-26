920c9f9ce202be0ed82e0ef48af4035318d69a14
KengoKudo <kng-kudo@yoshida-g.ac.jp>
2022-08-26 11:01:23 +0900

    ヘッダー位置の変更＆ul,liタグの変更

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -36,60 +36,59 @@
                 </button>
             </div>
         </div>
-        <div class="Video_List_Main">
-            <div class="Video_List">
-
-                <!-- $data が空の場合メッセージ出す -->
-                @foreach( $data as $datum )
-                    <div class="Video_List_Element_Container">
-                        <div class="Video_List_Element_Container_Left">
-                            <div class="Video_List_Element_PostingTime">
-                                投稿時間:{{ $datum['created_at'] }}
-                            </div>
-                            <div style="position: relative">
-                                <a href="/video/{{ $datum['id'] }}">
-                                    <img src="{{ asset($datum['thumbnail_path']) }}"
-                                         class="Video_List_Element_Thumbnail" alt="Thumbnail">
-                                </a>
-                                <span class="Video_List_Element_VideoTime">{{ $VideoTime -> time($datum['movie_path']) }}</span>
-                            </div>
+        <ul class="Video_List">
+            <!-- $data が空の場合メッセージ出す -->
+            @foreach( $data as $datum )
+                <li class="Video_List_Element_Container">
+                    <div class="Video_List_Element_Container_Left">
+                        <div class="Video_List_Element_PostingTime">
+                            投稿時間:{{ $datum['created_at'] }}
                         </div>
-                        <div class="Video_List_Element">
-                            <a href="/video/{{ $datum['id'] }}" class="Video_List_Element_Title">
-                                {{ $datum['title'] }}
+                        <div style="position: relative">
+                            <a href="/video/{{ $datum['id'] }}">
+                                <img src="{{ asset($datum['thumbnail_path']) }}"
+                                     class="Video_List_Element_Thumbnail" alt="Thumbnail">
                             </a>
-                            <div class="Video_List_Element_Overview">
-                                {{ $datum['comment'] }}
+                            <span
+                                class="Video_List_Element_VideoTime">{{ $VideoTime -> time($datum['movie_path']) }}</span>
+                        </div>
+                    </div>
+                    <div class="Video_List_Element">
+                        <a href="/video/{{ $datum['id'] }}" class="Video_List_Element_Title">
+                            {{ $datum['title'] }}
+                        </a>
+                        <div class="Video_List_Element_Overview">
+                            {{ $datum['comment'] }}
+                        </div>
+                        <div class="Video_List_Element_CommentList">
+                            <div class="Video_List_Element_Comment">
+                                コメント①
                             </div>
-                            <div class="Video_List_Element_CommentList">
-                                <div class="Video_List_Element_Comment">
-                                    コメント①
-                                </div>
-                                <div class="Video_List_Element_Comment">
-                                    コメント②
-                                </div>
-                            </div>
-                            <div class="Video_List_Element_List">
-                                <div class="Video_List_Element_Views">
-                                    再生数
-                                </div>
-                                <div class="Video_List_Element_NumberOfComments">
-                                    コメント数
-                                </div>
-                                <div class="Video_List_Element_NumberOfNice">
-                                    いいね数
-                                </div>
-                                <div class="Video_List_Element_NumberOfMyLists">
-                                    マイリスト数
-                                </div>
+                            <div class="Video_List_Element_Comment">
+                                コメント②
                             </div>
                         </div>
-                        <button type="button" class="Video_List_Element_Container_Edit_Button"
-                                onclick="location.href='/video_edit'">編集ボタン
-                        </button>
+                        <div class="Video_List_Element_List">
+                            <div class="Video_List_Element_Views">
+                                再生数
+                            </div>
+                            <div class="Video_List_Element_NumberOfComments">
+                                コメント数
+                            </div>
+                            <div class="Video_List_Element_NumberOfNice">
+                                いいね数
+                            </div>
+                            <div class="Video_List_Element_NumberOfMyLists">
+                                マイリスト数
+                            </div>
+                        </div>
                     </div>
-                @endforeach
-            </div>
-        </div>
+                    <button type="button" class="Video_List_Element_Container_Edit_Button"
+                            onclick="location.href='/video_edit'">編集ボタン
+                    </button>
+                </li>
+            @endforeach
+        </ul>
     </div>
+
 @endsection
