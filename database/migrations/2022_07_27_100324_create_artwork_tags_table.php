3a56261118a6c105efe5d170f4db7531b0593e9c
Hero <211014@st.yoshida-g.ac.jp>
2022-09-12 11:21:41 +0900

    お気に入り動画用のDB作成

--- database/migrations/2022_07_27_100324_create_artwork_tags_table.php
+++ database/migrations/2022_07_27_100324_create_artwork_tags_table.php
@@ -13,35 +13,10 @@ return new class extends Migration {
             $table->bigInteger('artwork_id')->unsigned()->nullable(false);
             $table->foreign('artwork_id')->references('id')->on('artworks')->cascadeOnDelete()->cascadeOnUpdate();
 
-            $table->bigInteger('tag1')->unsigned()->nullable(false);
-            $table->foreign('tag1')->references('id')->on('tags');
-
-            $table->bigInteger('tag2')->unsigned()->default(1);
-            $table->foreign('tag2')->references('id')->on('tags');
-
-            $table->bigInteger('tag3')->unsigned()->default(1);
-            $table->foreign('tag3')->references('id')->on('tags');
-
-            $table->bigInteger('tag4')->unsigned()->default(1);
-            $table->foreign('tag4')->references('id')->on('tags');
-
-            $table->bigInteger('tag5')->unsigned()->default(1);
-            $table->foreign('tag5')->references('id')->on('tags');
-
-            $table->bigInteger('tag6')->unsigned()->default(1);
-            $table->foreign('tag6')->references('id')->on('tags');
-
-            $table->bigInteger('tag7')->unsigned()->default(1);
-            $table->foreign('tag7')->references('id')->on('tags');
-
-            $table->bigInteger('tag8')->unsigned()->default(1);
-            $table->foreign('tag8')->references('id')->on('tags');
-
-            $table->bigInteger('tag9')->unsigned()->default(1);
-            $table->foreign('tag9')->references('id')->on('tags');
-
-            $table->bigInteger('tag10')->unsigned()->default(1);
-            $table->foreign('tag10')->references('id')->on('tags');
+            for ($i = 0; $i <= 10; $i++) {
+                $table->bigInteger("tag{$i}")->unsigned()->nullable(false);
+                $table->foreign("tag{$i}")->references('id')->on('tags');
+            }
 
             $table->dateTime('created_at');
             $table->dateTime('updated_at');
